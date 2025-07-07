65fb441dbec74f9cf6298da041c667279c8f9eb2
Andrei Kolchugin <gadm@688d294b-033f-3e32-b8a5-18f0780f0688>
2025-07-07 23:37:17 +0300

    Starting Arduino class build

---
+++ src/CircStringBuf.cpp
@@ -0,0 +1,40 @@
+/*
+ * CircStringBuf -- library implements fast circular buffer for C-style
+ *                  (i.e, '\0'-terminated) strings.
+ *
+ * Arduino Class
+ *
+ * Copyright © 2019 sven-hm@github
+ * Copyright © 2025 Andrei Kolchugin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+CircularStringBuffer::CircularStringBuffer(size_t size)
+	: bufferCtl({NULL, 0, 0, 0, true}) {
+
+	if (buffer = malloc(size)) {
+
+		bufferCtl.start = buffer;
+		bufferCtl.end = size;
+	}
+}
+
+CircularStringBuffer::~CircularStringBuffer() {
+
+	if (buffer)
+		free(buffer);
+}
+
---
+++ src/CircStringBuf.h
@@ -0,0 +1,74 @@
+/*
+ * CircStringBuf -- library implements fast circular buffer for C-style
+ *                  (i.e, '\0'-terminated) strings.
+ *
+ * Arduino Class
+ *
+ * Copyright © 2019 sven-hm@github
+ * Copyright © 2025 Andrei Kolchugin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+/* Rudimentary form of configuration */
+#if defined(HAVE_CONFIG_H)
+#	include "config.h"
+#else /* defined(HAVE_CONFIG_H) */
+#	if __has_include("config.h")
+#		include "config.h"
+#	endif /* __has_include("config.h") */
+#endif /* defined(HAVE_CONFIG_H) */
+
+#include <ThreadCompat.h>
+#include <circstringbuf/circstringbuf.h>
+
+class CircularStringBuffer {
+
+public:
+
+	CircularStringBuffer() = delete;
+	CircularStringBuffer(size_t size);
+	~CircularStringBuffer();
+
+protected:
+
+	thread::Mutex mtxCSBuffer;
+
+private:
+
+	circstringbuf_t bufferCtl;
+	char *buffer;
+};
+
+template<size_t BufferSize>
+class StaticCircularStringBuffer {
+
+public:
+
+	CircularStringBuffer() : bufferCtl({buffer, BufferSize, 0, 0, true}) {}
+	~CircularStringBuffer() {}
+
+protected:
+
+	thread::Mutex mtxCSBuffer;
+
+private:
+
+	circstringbuf_t bufferCtl;
+	char *buffer[BufferSize];
+};
+
